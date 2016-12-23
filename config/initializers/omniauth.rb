01783c77a23d5b9ea292b3a1a37ee93cfd83dad3
twiggyweasel <orsusbass@gmail.com>
2016-12-23 20:19:21 +0000

    omniauth var tweets

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_KEY']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :identity, fields: [:email, :name], model: User, on_failed_registration: lambda { |env|      
     UsersController.action(:new).call(env)  
   }
